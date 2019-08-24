fcd3c90a171d810c0a94847e043873b551d7a8b2
Hwan <dnfwlq8054@naver.com>
2019-08-25 02:34:56 +0900

    commit

---
+++ ubunt_netReset.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 1024
+
+int main(){
+    char buff[BUFF_SIZE];
+    char ip_addr[100] = {'x', };
+    FILE *fp;
+    char *ptr;
+
+    while(1){
+        if(NULL == (fp = popen("ip route", "r"))){
+            perror("popen error!!");
+            return -1;
+        }
+
+
+        while(fgets(buff, BUFF_SIZE, fp)){
+            ptr = strtok(buff, " ");
+    
+            if(ip_addr[0] != 'x')
+                break;
+    
+            while(1){
+                ptr = strtok(NULL, " ");
+    
+                if(0 == (strcmp("via", ptr))){
+                    ptr = strtok(NULL, " ");
+                    strcpy(ip_addr, ptr);
+                    break;
+                }
+            }//while() 
+        }//while()
+    
+
+        if(ip_addr[0] == 'x'){
+            printf("network restart....\n");
+            if(NULL == (fp = popen("service network-manager restart", "r"))){
+                perror("popen error!!");
+                return -1;
+            }
+        }else
+            printf("good network!!\n");
+        sleep(3);
+        memset(ip_addr, 0, 100);
+        ip_addr[0] = 'x';
+        pclose(fp);
+    }
+
+    return 0;
+}
+
